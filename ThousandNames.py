25cf3df124e0b4d93531a7918d03881fbc95ce66
Tangle10 <bfflinko@gmail.com>
2016-08-21 22:47:11 -0400

    Vastly simplified with help of /u/jedwardsol

--- ThousandNames.py
+++ ThousandNames.py
@@ -67,40 +67,20 @@ english_dict = {
     59: "'s",
 }
 
+# this function grabs syllables from the dictionary and strings them together.
 word = 0
 wordcount = 0
-word0 = 0
-word1 = 0
-word2 = 0
-word3 = 0
-word4 = 0
-word5 = 0
-word6 = 0
-word7 = 0
-word8 = 0
-word9 = 0
-word10 = 0
 while wordcount < 1000:
+    sylc2 = 0
     sylc = random.randrange(2,8)
-    syl1 = english_dict[random.randrange(0,59)]
-    syl2 = english_dict[random.randrange(0,59)]
-    syl3 = english_dict[random.randrange(0,59)]
-    syl4 = english_dict[random.randrange(0,59)]
-    syl5 = english_dict[random.randrange(0,59)]
-    syl6 = english_dict[random.randrange(0,59)]
-    syl7 = english_dict[random.randrange(0,59)]
-    syl8 = english_dict[random.randrange(0,59)]
-    if sylc == 1: word = str(syl1) + str(syl2)
-    elif sylc == 2: word = str(syl1) + str(syl2)
-    elif sylc == 3: word = str(syl1) + str(syl2) + str(syl3)
-    elif sylc == 4: word = str(syl1) + str(syl2) + str(syl3) + str(syl4)
-    elif sylc == 5: word = str(syl1) + str(syl2) + str(syl3) + str(syl4) + str(syl5)
-    elif sylc == 6: word = str(syl1) + str(syl2) + str(syl3) + str(syl4) + str(syl5) + str(syl6)
-    elif sylc == 7: word = str(syl1) + str(syl2) + str(syl3) + str(syl4) + str(syl5) + str(syl6) + str(syl7)
-    elif sylc == 8: word = str(syl1) + str(syl2) + str(syl3) + str(syl4) + str(syl5) + str(syl7) + str(syl8)
-    #attempt to create "word0", "word1", etc.
+    while sylc2 < sylc
+        syla = english_dict[random.randrange(0,59)]
+        word = word+str(syla)
+        sylc2 = sylc2 + 1
+    # shows it on the screen so you can laugh at it
     print str(word)
     words.append(word)
     wordcount = wordcount + 1
+# this part creates an output
 with open("TrillionNames" + str(RAND) + ".txt", "w") as text_file:
     text_file.write(str(words))
